68845845c17568b73e9b826ff9c86c63b5e11a53
anna-jonsson <anna.f.jonsson@gmail.com>
2022-05-03 11:55:05 +0200

    Further work on menu
    See files for addition. Among:
    
    Added correct dimensions for menu elements.

--- JS/homepage.js
+++ JS/homepage.js
@@ -4,28 +4,28 @@ let menu = document.getElementById("menu");
 
 const menuElements = [
 
-    {
+    { //Home
         icon: "../Images/Icons/home.png",
         iconClicked: "../Images/Icons/home-clicked.png",
         url: "Home.html" 
         
     },
 
-    {
+    { //Search
         icon: "../Images/Icons/search.png",
         iconClicked: "../Images/Icons/search-clicked.png",
         url: "Search.html"
         
     },
 
-    {
+    { //Contact
         icon: "../Images/Icons/about.png", //will be changed to @ sign
         iconClicked: "../Images/Icons/about-clicked.png",
         url: "Contact.html"
         
     },
 
-    {
+    { //FAQ
         icon: "../Images/Icons/faq.png",
         iconClicked: "../Images/Icons/faq-clicked.png",
         url: "FAQ.html"
@@ -33,14 +33,35 @@ const menuElements = [
     },
 ]
 
+function currentURL (filename) {
+    let path = window.location.pathname;
+    let page = path.split("/").pop();
+
+
+    if (page == filename) {
+        return true;
+    } 
+}
+
 function buildMenu () { 
     
     for (let i = 0 ; i < 4 ; i++) {
 
+        let checker = currentURL("Home.html");
+
         let div = document.createElement("div"); 
-        div.innerHTML = `<img src="${menuElements[i].icon}">`
-        div.classList.add("menuElement")
+        if (checker == true) {
+            div.innerHTML = `<img src="${menuElements[i].iconClicked}">`
+        } else {
+            div.innerHTML = `<img src="${menuElements[i].icon}">`
+        }
         
+        div.classList.add("menuElement");
         menu.appendChild(div);
+
     }
+
+
 }
+
+buildMenu()
