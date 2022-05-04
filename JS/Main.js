5aa2f702cbc5e5b38713dd0e06f0ab6b2fdea07b
My Bergquist Rajic <mybergqvist@gmail.com>
2022-05-04 12:52:06 +0200

    Added comments and fixed small issues

--- JS/Main.js
+++ JS/Main.js
@@ -4,9 +4,10 @@
 
 let countryBtn = document.getElementById("countries-btn");
 let cityBtn = document.getElementById("cities-btn");
-let programmesBtn = document.getElementById("courses-btn");
+let programmeBtn = document.getElementById("courses-btn");
 
-function addEventHandlers () {
+// Event handlers for country, city, programme buttons
+function addEventHandlers() {
 
     countryBtn.addEventListener("click", function () {
         countryBtn.classList.toggle("active");
@@ -16,52 +17,55 @@ function addEventHandlers () {
         cityBtn.classList.toggle("active");
     })
 
-    programmesBtn.addEventListener("click", function () {
-        programmesBtn.classList.toggle("active");
+    programmeBtn.addEventListener("click", function () {
+        programmeBtn.classList.toggle("active");
     })
 }
 
-function getResults () {
+function getResults() {
 
 }
 
 //Untoggled fetch results for each category (COUNTRIES, CITIES, PROGRAMMES)
 
-function unToggled () {
+function unToggled() {
     const DATABASE = [];
- 
-    DATABASE.push(COUNTRIES, CITIES, PROGRAMMES)
- 
-    return DATABASE;
- }
 
-function getAllCountries () {
+    DATABASE.push(COUNTRIES, CITIES, PROGRAMMES)
+
+    return DATABASE;
+}
+
+// Stores ALL countries in an array
+function getAllCountries() {
 
     const allCountries = [];
 
-    for (let i = 0 ; i < COUNTRIES.length ; i++) {
+    for (let i = 0; i < COUNTRIES.length; i++) {
         allCountries.push(COUNTRIES[i]);
     }
 
     return allCountries;
 }
 
-function getAllCities () {
+// Stores ALL cities in an array
+function getAllCities() {
 
     const allCities = [];
 
-    for (let i = 0 ; i < CITIES.length ; i++) {
+    for (let i = 0; i < CITIES.length; i++) {
         allCities.push(CITIES[i]);
     }
 
     return allCities;
 }
 
-function getAllProgrammes () {
+// Stores ALL programmes in an array
+function getAllProgrammes() {
 
     const allProgrammes = [];
 
-    for (let i = 0 ; i < PROGRAMMES.length ; i++) {
+    for (let i = 0; i < PROGRAMMES.length; i++) {
         allProgrammes.push(PROGRAMMES[i]);
     }
 
@@ -69,21 +73,24 @@ function getAllProgrammes () {
 }
 
 //Toggle results for each category (COUNTRIES, CITIES, PROGRAMMES)
+function toggleOptions() {
 
-function toggleOptions () {
-    
     let results = [];
 
     if (countryBtn.classList.contains("active") == true) {
-        results = [getAllCountries()]
-    } 
+        results = [...getAllCountries()]
+    }
 
     if (cityBtn.classList.contains("active") == true) {
         results = [...results, ...getAllCities()]
     }
 
-    if (programmesBtn.classList.contains("active") == true) {
+    if (programmeBtn.classList.contains("active") == true) {
         results = [...results, ...getAllProgrammes()]
+    }
+
+    if (results.length == 0) {
+        results = [...getAllCountries(), ...getAllCities(), ...getAllProgrammes()]
     }
 
     return results;
