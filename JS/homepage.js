16e8eb9cec8636c35b8f02aec6ac76c4b4c4b395
My Bergquist Rajic <mybergqvist@gmail.com>
2022-05-05 11:04:37 +0200

    Fixed menu

--- JS/homepage.js
+++ JS/homepage.js
@@ -7,55 +7,68 @@ const menuElements = [
     { //Home
         icon: "../Images/Icons/home.png",
         iconClicked: "../Images/Icons/home-clicked.png",
-        url: "Home.html" 
-        
+        url: "Home.html"
+
     },
 
     { //Search
         icon: "../Images/Icons/search.png",
         iconClicked: "../Images/Icons/search-clicked.png",
         url: "Search.html"
-        
+
     },
 
     { //Contact
         icon: "../Images/Icons/about.png", //will be changed to @ sign
         iconClicked: "../Images/Icons/about-clicked.png",
         url: "Contact.html"
-        
+
     },
 
     { //FAQ
         icon: "../Images/Icons/faq.png",
         iconClicked: "../Images/Icons/faq-clicked.png",
         url: "FAQ.html"
-        
+
     },
 ]
 
-function currentURL (filename) {
+// function currentURL(filename) {
+//     let path = window.location.pathname;
+//     let page = path.split("/").pop();
+//     console.log(page.split(".")[0].toLowerCase());
+
+//     if (page == filename && page.split(".")[0].toLowerCase() == menuElements[i]) {
+//         return true;
+//     }
+// }
+
+
+function check(i) {
     let path = window.location.pathname;
-    let page = path.split("/").pop();
+    let pagename = path.split("/").pop().split(".")[0].toLocaleLowerCase();
+    let image = menuElements[i].icon;
+    let imagename = image.split("/").pop().split(".")[0];
 
-
-    if (page == filename) {
-        return true;
-    } 
+    if (pagename == imagename) {
+        return true
+    }
 }
 
-function buildMenu () { 
-    
-    for (let i = 0 ; i < 4 ; i++) {
 
-        let checker = currentURL("Home.html");
+function buildMenu() {
 
-        let div = document.createElement("div"); 
+    for (let i = 0; i < 4; i++) {
+
+        let checker = check(i);
+
+        let div = document.createElement("div");
         if (checker == true) {
             div.innerHTML = `<img src="${menuElements[i].iconClicked}">`
         } else {
             div.innerHTML = `<img src="${menuElements[i].icon}">`
         }
-        
+
         div.classList.add("menuElement");
         menu.appendChild(div);
 
