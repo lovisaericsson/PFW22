729e7417939a91e220310ce477947f8c2b9d1f39
lovisaericsson <lovisa.ericsson01@gmail.com>
2022-05-06 15:43:21 +0200

    Fixed Questions in JS-file

---
+++ JS/FAQ.js
@@ -0,0 +1,52 @@
+"use strict"
+
+const Questions = [
+    { "id": 0,
+      "title": "Kan jag åka på utbyte inom mitt program?" 
+    },
+    { "id": 1,
+      "title": "När får man besked om vilken plats man har fått?" 
+    },
+    { "id": 2,
+      "title": "Kan jag få studiemedel från CSN för utlandsstudier?" 
+    },
+    { "id": 3,
+      "title": "Vad är det som avgör om jag får en plats?" 
+    },
+    { "id": 4,
+    "title": "Hur många universitet måste jag söka för att vara säker på att få en plats?" 
+    },
+    { "id": 5,
+    "title": "Jag har bytt program, hur fungerar det med behörighet och betygssnitt?" 
+    },
+    { "id": 6,
+    "title": "När kan jag åka på utbyte?" 
+    },
+    { "id": 7,
+    "title": "Vad händer om jag inte klarar kurserna på utbytet?" 
+    },
+    { "id": 8,
+    "title": "Är jag försäkrad under utbytet?" 
+    },
+    { "id": 9,
+    "title": "Vart ska jag vända mig ifall jag råka ut för en olycka eller något händer?" 
+    },
+    { "id": 10,
+    "title": "Behöver jag göra ett språktest?" 
+    },
+    { "id": 11,
+    "title": "Kan jag få åka på utbyte med en kompis?"
+    },
+    { "id": 12,
+    "title": "Vad är ett learning agreement?" 
+    },
+    { "id": 13,
+    "title": "Hur gör jag med boende?" 
+    },
+    { "id": 14,
+    "title": "Kan jag jobba medan jag studera i USA?" 
+    },
+    { "id": 15,
+    "title": "Vad händer om jag blir antagen till en utbildning men jag ångrar mig?" 
+    },
+];
