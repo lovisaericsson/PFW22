c5c48c68b75e200ebce1f3b54241666a2ecae44b
lovisaericsson <lovisa.ericsson01@gmail.com>
2022-05-06 15:47:42 +0200

    Fixed answers on the JS-file

--- JS/FAQ.js
+++ JS/FAQ.js
@@ -50,3 +50,55 @@ const Questions = [
     "title": "Vad händer om jag blir antagen till en utbildning men jag ångrar mig?" 
     },
 ];
+
+
+const Answers = [
+    { "id": 0,
+      "title": "Vänd dig till den internationella kontaktpersonen vid ditt program för att få information om möjligheten att åka på utbyte inom ditt program."  
+    },
+    { "id": 1,
+      "title": "Du får besked via mail några veckor efter sista ansökningsdag."
+    },
+    { "id": 2,
+    "title": "Ja, du kan ansöka om studiemedel från CSN som vanligt för din utbytestermin."
+    },
+    { "id": 3,
+    "title": "Studieplanen måste stämma överens med universitetsinformationen i SoleMove. Därefter högst betygssnitt. Om flera studenter har samma meritvärde avgörs urvalet genom en samlad bedömning av ansökan där störst vikt läggs vid motivationsbrev (Statement of Purpose) och CV."
+    },
+    { "id": 4,
+    "title": "Vi brukar kunna tilldela platser till 80% av våra sökande studenter, så chanserna är goda. Ju fler universitet du söker desto större är chanserna att få en plats, men sök bara de universitet du verkligen vill åka till."
+    },
+    { "id": 5,
+    "title": "Första årets studier på ditt nuvarande program måste vara helt avklarade för att du ska vara behörig. Förutom ÅK1 prövas behörigheten utifrån den individuella studieplan som upprättas i samband med antagning till nuvarande program.Betygssnittet beräknas på avklarade och tillgodoräknade kurser med TH-skala inom ditt aktuella program." 
+    },
+    { "id": 6,
+    "title": "Prata med ditt program om när det bäst passar att åka på utbyte."
+    },
+    { "id": 7,
+    "title": "Om det blir aktuellt ska du ta reda på vilka möjligheter det finns för att tenta om eller komplettera för att få godkänt. Du måste även förhålla dig till eventuella regler vad gäller exempelvis CSN och Erasmus. Du förväntas studera på heltid det vill säga motsvarande 30 hp per termin."
+    },
+    { "id": 8,
+    "title": "Ja, alla studenter är försäkrade under utbytet."
+    },
+    { "id": 9,
+    "title": "Vid allvarlig sjukdom, intagning på sjukhus eller behov av annan aktiv skadehjälp utanför Norden, skall omedelbar kontakt tas med Falck Global Assistance"
+    },
+    { "id": 10,
+    "title": "Vissa universitet har krav på språktest. Exempelvis kan utbytesstudenter i vissa fall behöva skriva ett Test of English as a Foreign Language, TOEFL."
+    },
+    { "id": 11,
+    "title": "Det går inte att söka utbytesstudier tillsammans. Däremot kan ni ansöka till samma utbyteslärosäte men utbytesplatserna blir tilldelade utifrån ett meritvärde som räknas ut i placeringsprocessen."
+    },
+    { "id": 12,
+    "title": "Ett Learning agreement måste alla studenter fylla i innan de åker på utbytesstudier. Det är ett kontrakt mellan dig, ansvarig för tillgodoräkning och utbytesuniversitetet om din studieplan för din termin."
+    },
+    { "id": 13,
+    "title": "Många skolor erbjuder alternativ för studentboende. I vissa fall finns boende på campus, medan man i andra fall söker lägenhet i stan på egen hand. Vi informerar gärna om de olika alternativen och kommer med tips och råd inför flytten"
+    },
+    { "id": 14,
+    "title": " USA får du generellt sett inte arbeta när du är där som student. Det finns dock undantag och det går att arbeta på skolans campusområde eller på praktikplatser (internships)."
+    },
+    { "id": 15,
+    "title": "Det är respektive college/universitets villkor som gäller för återbetalning och avgifter vid exempelvis avbokning och uppskjutande av studier och det är viktigt att du tar del av skolans villkor i samband med att du gör din ansökan. Vi försöker dock alltid hjälpa dig om det skulle uppstå några problem. Det är viktigt att du kontaktar oss så snart som möjligt i sådana fall."
+    },
+];
