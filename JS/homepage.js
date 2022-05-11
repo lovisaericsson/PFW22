c2184c7e46bb73ccff6fd14f64153737692be325
anna-jonsson <anna.f.jonsson@gmail.com>
2022-05-11 12:09:52 +0200

    Menu fixes
    New js-file for menu, linked to all other js-files
    
    New css-file for menu, linked to all other html files
    
    Fixed links to menu.js and menu.css in all html files

--- JS/homepage.js
+++ JS/homepage.js
@@ -1,66 +1,7 @@
 //function for building menu
 
 let body = document.querySelector("body");
-let menu = document.getElementById("menu");
 let background = document.getElementById("background");
-
-const menuElements = [
-
-    { //Home
-        icon: "../Images/Icons/home.png",
-        iconClicked: "../Images/Icons/home-clicked.png",
-        url: "Home.html"
-
-    },
-
-    { //Search
-        icon: "../Images/Icons/search.png",
-        iconClicked: "../Images/Icons/search-clicked.png",
-        url: "Search.html"
-
-    },
-
-    { //Contact
-        icon: "../Images/Icons/contact.png",
-        iconClicked: "../Images/Icons/about-clicked.png", //Will be changed to "clicked" mail
-        url: "Contact.html"
-
-    },
-
-    { //FAQ
-        icon: "../Images/Icons/faq.png",
-        iconClicked: "../Images/Icons/faq-clicked.png",
-        url: "FAQ.html"
-
-    },
-]
-
-// Checks is html-name is the same as image-name
-function check(i) {
-    let path = window.location.pathname;
-    let pagename = path.split("/").pop().split(".")[0].toLocaleLowerCase();
-    let image = menuElements[i].icon;
-    let imagename = image.split("/").pop().split(".")[0];
-
-    if (pagename == imagename) {
-        return true
-    }
-}
-
-// Builds menu
-function buildMenu() {
-    for (let i = 0; i < 4; i++) {
-        let checker = check(i);
-        let div = document.createElement("div");
-        if (checker == true) {
-            div.innerHTML = `<a href="${menuElements[i].url}"> <img src="${menuElements[i].iconClicked}"> </a>`
-        } else {
-            div.innerHTML = `<a href="${menuElements[i].url}"> <img src="${menuElements[i].icon}"> </a>`
-        }
-        div.classList.add("menuElement");
-        menu.appendChild(div);
-    }
-}
 
 
 function setBackground () {
@@ -105,7 +46,5 @@ function runBackground () {
     setBgShape();
     setIcon();
 }
-
-buildMenu();
 
 runBackground();
