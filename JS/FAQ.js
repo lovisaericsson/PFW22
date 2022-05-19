b0f50850fec838f62a52d5bbc1c5dc75f7a9e6b8
anna-jonsson <anna.f.jonsson@gmail.com>
2022-05-19 11:50:00 +0200

    Formatting, adding, deleting

--- JS/FAQ.js
+++ JS/FAQ.js
@@ -1,209 +1,230 @@
-"use strict"
+'use strict'
 
 const Questions = [
   {
-    "id": 0,
-    "title": "Kan jag åka på utbyte inom mitt program?"
+    id: 0,
+    title: 'Kan jag åka på utbyte inom mitt program?'
   },
   {
-    "id": 1,
-    "title": "När får man besked om vilken plats man har fått?"
+    id: 1,
+    title: 'När får man besked om vilken plats man har fått?'
   },
   {
-    "id": 2,
-    "title": "Kan jag få studiemedel från CSN för utlandsstudier?"
+    id: 2,
+    title: 'Kan jag få studiemedel från CSN för utlandsstudier?'
   },
   {
-    "id": 3,
-    "title": "Vad är det som avgör om jag får en plats?"
+    id: 3,
+    title: 'Vad är det som avgör om jag får en plats?'
   },
   {
-    "id": 4,
-    "title": "Hur många universitet måste jag söka för att vara säker på att få en plats?"
+    id: 4,
+    title:
+      'Hur många universitet måste jag söka för att vara säker på att få en plats?'
   },
   {
-    "id": 5,
-    "title": "Jag har bytt program, hur fungerar det med behörighet och betygssnitt?"
+    id: 5,
+    title:
+      'Jag har bytt program, hur fungerar det med behörighet och betygssnitt?'
   },
   {
-    "id": 6,
-    "title": "När kan jag åka på utbyte?"
+    id: 6,
+    title: 'När kan jag åka på utbyte?'
   },
   {
-    "id": 7,
-    "title": "Vad händer om jag inte klarar kurserna på utbytet?"
+    id: 7,
+    title: 'Vad händer om jag inte klarar kurserna på utbytet?'
   },
   {
-    "id": 8,
-    "title": "Är jag försäkrad under utbytet?"
+    id: 8,
+    title: 'Är jag försäkrad under utbytet?'
   },
   {
-    "id": 9,
-    "title": "Vart ska jag vända mig ifall jag råka ut för en olycka eller något händer?"
+    id: 9,
+    title:
+      'Vart ska jag vända mig ifall jag råka ut för en olycka eller något händer?'
   },
   {
-    "id": 10,
-    "title": "Behöver jag göra ett språktest?"
+    id: 10,
+    title: 'Behöver jag göra ett språktest?'
   },
   {
-    "id": 11,
-    "title": "Kan jag få åka på utbyte med en kompis?"
+    id: 11,
+    title: 'Kan jag få åka på utbyte med en kompis?'
   },
   {
-    "id": 12,
-    "title": "Vad är ett learning agreement?"
+    id: 12,
+    title: 'Vad är ett learning agreement?'
   },
   {
-    "id": 13,
-    "title": "Hur gör jag med boende?"
+    id: 13,
+    title: 'Hur gör jag med boende?'
   },
   {
-    "id": 14,
-    "title": "Kan jag jobba medan jag studera i USA?"
+    id: 14,
+    title: 'Kan jag jobba medan jag studera i USA?'
   },
   {
-    "id": 15,
-    "title": "Vad händer om jag blir antagen till en utbildning men jag ångrar mig?"
-  },
-];
-
+    id: 15,
+    title:
+      'Vad händer om jag blir antagen till en utbildning men jag ångrar mig?'
+  }
+]
 
 const Answers = [
   {
-    "id": 0,
-    "title": "Vänd dig till den internationella kontaktpersonen vid ditt program för att få information om möjligheten att åka på utbyte inom ditt program."
+    id: 0,
+    title:
+      'Vänd dig till den internationella kontaktpersonen vid ditt program för att få information om möjligheten att åka på utbyte inom ditt program.'
   },
   {
-    "id": 1,
-    "title": "Du får besked via mail några veckor efter sista ansökningsdag."
+    id: 1,
+    title: 'Du får besked via mail några veckor efter sista ansökningsdag.'
   },
   {
-    "id": 2,
-    "title": "Ja, du kan ansöka om studiemedel från CSN som vanligt för din utbytestermin."
+    id: 2,
+    title:
+      'Ja, du kan ansöka om studiemedel från CSN som vanligt för din utbytestermin.'
   },
   {
-    "id": 3,
-    "title": "Studieplanen måste stämma överens med universitetsinformationen i SoleMove. Därefter högst betygssnitt. Om flera studenter har samma meritvärde avgörs urvalet genom en samlad bedömning av ansökan där störst vikt läggs vid motivationsbrev (Statement of Purpose) och CV."
+    id: 3,
+    title:
+      'Studieplanen måste stämma överens med universitetsinformationen i SoleMove. Därefter högst betygssnitt. Om flera studenter har samma meritvärde avgörs urvalet genom en samlad bedömning av ansökan där störst vikt läggs vid motivationsbrev (Statement of Purpose) och CV.'
   },
   {
-    "id": 4,
-    "title": "Vi brukar kunna tilldela platser till 80% av våra sökande studenter, så chanserna är goda. Ju fler universitet du söker desto större är chanserna att få en plats, men sök bara de universitet du verkligen vill åka till."
+    id: 4,
+    title:
+      'Vi brukar kunna tilldela platser till 80% av våra sökande studenter, så chanserna är goda. Ju fler universitet du söker desto större är chanserna att få en plats, men sök bara de universitet du verkligen vill åka till.'
   },
   {
-    "id": 5,
-    "title": "Första årets studier på ditt nuvarande program måste vara helt avklarade för att du ska vara behörig. Förutom ÅK1 prövas behörigheten utifrån den individuella studieplan som upprättas i samband med antagning till nuvarande program.Betygssnittet beräknas på avklarade och tillgodoräknade kurser med TH-skala inom ditt aktuella program."
+    id: 5,
+    title:
+      'Första årets studier på ditt nuvarande program måste vara helt avklarade för att du ska vara behörig. Förutom ÅK1 prövas behörigheten utifrån den individuella studieplan som upprättas i samband med antagning till nuvarande program.Betygssnittet beräknas på avklarade och tillgodoräknade kurser med TH-skala inom ditt aktuella program.'
   },
   {
-    "id": 6,
-    "title": "Prata med ditt program om när det bäst passar att åka på utbyte."
+    id: 6,
+    title: 'Prata med ditt program om när det bäst passar att åka på utbyte.'
   },
   {
-    "id": 7,
-    "title": "Om det blir aktuellt ska du ta reda på vilka möjligheter det finns för att tenta om eller komplettera för att få godkänt. Du måste även förhålla dig till eventuella regler vad gäller exempelvis CSN och Erasmus. Du förväntas studera på heltid det vill säga motsvarande 30 hp per termin."
+    id: 7,
+    title:
+      'Om det blir aktuellt ska du ta reda på vilka möjligheter det finns för att tenta om eller komplettera för att få godkänt. Du måste även förhålla dig till eventuella regler vad gäller exempelvis CSN och Erasmus. Du förväntas studera på heltid det vill säga motsvarande 30 hp per termin.'
   },
   {
-    "id": 8,
-    "title": "Ja, alla studenter är försäkrade under utbytet."
+    id: 8,
+    title: 'Ja, alla studenter är försäkrade under utbytet.'
   },
   {
-    "id": 9,
-    "title": "Vid allvarlig sjukdom, intagning på sjukhus eller behov av annan aktiv skadehjälp utanför Norden, skall omedelbar kontakt tas med Falck Global Assistance"
+    id: 9,
+    title:
+      'Vid allvarlig sjukdom, intagning på sjukhus eller behov av annan aktiv skadehjälp utanför Norden, skall omedelbar kontakt tas med Falck Global Assistance'
   },
   {
-    "id": 10,
-    "title": "Vissa universitet har krav på språktest. Exempelvis kan utbytesstudenter i vissa fall behöva skriva ett Test of English as a Foreign Language, TOEFL."
+    id: 10,
+    title:
+      'Vissa universitet har krav på språktest. Exempelvis kan utbytesstudenter i vissa fall behöva skriva ett Test of English as a Foreign Language, TOEFL.'
   },
   {
-    "id": 11,
-    "title": "Det går inte att söka utbytesstudier tillsammans. Däremot kan ni ansöka till samma utbyteslärosäte men utbytesplatserna blir tilldelade utifrån ett meritvärde som räknas ut i placeringsprocessen."
+    id: 11,
+    title:
+      'Det går inte att söka utbytesstudier tillsammans. Däremot kan ni ansöka till samma utbyteslärosäte men utbytesplatserna blir tilldelade utifrån ett meritvärde som räknas ut i placeringsprocessen.'
   },
   {
-    "id": 12,
-    "title": "Ett Learning agreement måste alla studenter fylla i innan de åker på utbytesstudier. Det är ett kontrakt mellan dig, ansvarig för tillgodoräkning och utbytesuniversitetet om din studieplan för din termin."
+    id: 12,
+    title:
+      'Ett Learning agreement måste alla studenter fylla i innan de åker på utbytesstudier. Det är ett kontrakt mellan dig, ansvarig för tillgodoräkning och utbytesuniversitetet om din studieplan för din termin.'
   },
   {
-    "id": 13,
-    "title": "Många skolor erbjuder alternativ för studentboende. I vissa fall finns boende på campus, medan man i andra fall söker lägenhet i stan på egen hand. Vi informerar gärna om de olika alternativen och kommer med tips och råd inför flytten"
+    id: 13,
+    title:
+      'Många skolor erbjuder alternativ för studentboende. I vissa fall finns boende på campus, medan man i andra fall söker lägenhet i stan på egen hand. Vi informerar gärna om de olika alternativen och kommer med tips och råd inför flytten'
   },
   {
-    "id": 14,
-    "title": " USA får du generellt sett inte arbeta när du är där som student. Det finns dock undantag och det går att arbeta på skolans campusområde eller på praktikplatser (internships)."
+    id: 14,
+    title:
+      ' USA får du generellt sett inte arbeta när du är där som student. Det finns dock undantag och det går att arbeta på skolans campusområde eller på praktikplatser (internships).'
   },
   {
-    "id": 15,
-    "title": "Det är respektive college/universitets villkor som gäller för återbetalning och avgifter vid exempelvis avbokning och uppskjutande av studier och det är viktigt att du tar del av skolans villkor i samband med att du gör din ansökan. Vi försöker dock alltid hjälpa dig om det skulle uppstå några problem. Det är viktigt att du kontaktar oss så snart som möjligt i sådana fall."
-  },
-];
+    id: 15,
+    title:
+      'Det är respektive college/universitets villkor som gäller för återbetalning och avgifter vid exempelvis avbokning och uppskjutande av studier och det är viktigt att du tar del av skolans villkor i samband med att du gör din ansökan. Vi försöker dock alltid hjälpa dig om det skulle uppstå några problem. Det är viktigt att du kontaktar oss så snart som möjligt i sådana fall.'
+  }
+]
 
-function renderQuestions() {
-  let wrapper = document.getElementById("faq");
-  wrapper.innerHTML = ""
+//Renders questions and answers into HTML (also including advertisements)
+function renderQuestions () {
+  let wrapper = document.getElementById('faq')
+  wrapper.innerHTML = ''
   for (let j = 0; j < Questions.length; j++) {
     if (j != 0 && j % 4 == 0) {
       renderAdvertisement(wrapper)
     }
-    createDiv(Questions[j].title, Answers[j].title, wrapper);
-
+    createDiv(Questions[j].title, Answers[j].title, wrapper)
   }
 }
 
-function createDiv(title, answer, wrapper) {
-  let createDiv = document.createElement("div");
-  createDiv.classList.add("box");
-  createDiv.innerHTML = `<p>${title}</p>`;
+//Function outlining template for creating the question HTML elements included in the FAQ
+//(rendered in renderQuestions).
+function createDiv (title, answer, wrapper) {
+  let createDiv = document.createElement('div')
+  createDiv.classList.add('box')
+  createDiv.innerHTML = `<p>${title}</p>`
   // "initializes" arrow
-  createDiv.classList.add("faq-arrow-down");
-  createDiv.style.backgroundImage = "url('../Images/arrow-down.png')";
-  wrapper.appendChild(createDiv);
+  createDiv.classList.add('faq-arrow-down')
+  createDiv.style.backgroundImage = "url('../Images/arrow-down.png')"
+  wrapper.appendChild(createDiv)
 
-  let divWithFaq = createAnswer(answer);
-  createDiv.appendChild(divWithFaq);
-  createDiv.addEventListener("click", function () {
-    divWithFaq.classList.toggle("answer-result");
-    if (divWithFaq.classList.contains("answer-result")) {
-      createDiv.classList.add("faq-arrow-up");
-      createDiv.classList.remove("faq-arrow-down");
-      createDiv.style.backgroundImage = "url('../Images/arrow-up.png')";
+  let divWithFaq = createAnswer(answer)
+  createDiv.appendChild(divWithFaq)
+
+  //event listener for drop-down:s (answers to Q:s)
+  createDiv.addEventListener('click', function () {
+    divWithFaq.classList.toggle('answer-result')
+    if (divWithFaq.classList.contains('answer-result')) {
+      createDiv.classList.add('faq-arrow-up')
+      createDiv.classList.remove('faq-arrow-down')
+      createDiv.style.backgroundImage = "url('../Images/arrow-up.png')"
 
       //FAQ automatically scrolls up to display whole answer
-      createDiv.scrollIntoView({block: "center"})
+      createDiv.scrollIntoView({ block: 'center' })
     }
     // When not clicked, show arrow down
     else {
-      createDiv.classList.remove("faq-arrow-up");
-      createDiv.classList.add("faq-arrow-down");
-      createDiv.style.backgroundImage = "url('../Images/arrow-down.png')";
+      createDiv.classList.remove('faq-arrow-up')
+      createDiv.classList.add('faq-arrow-down')
+      createDiv.style.backgroundImage = "url('../Images/arrow-down.png')"
     }
   })
-
 
   return createDiv
 }
 
-function createAnswer(answer) {
-  let answerContainer = document.createElement("div");
-  answerContainer.classList.add("hidden-answer-result");
-  let answerDiv = document.createElement("div");
+//Function outlining template for creating the answer HTML elements included in the FAQ
+//(rendered in renderQuestions).
+function createAnswer (answer) {
+  let answerContainer = document.createElement('div')
+  answerContainer.classList.add('hidden-answer-result')
+  let answerDiv = document.createElement('div')
   answerDiv.innerHTML = answer
-  answerContainer.appendChild(answerDiv);
+  answerContainer.appendChild(answerDiv)
 
   return answerContainer
 }
 
 // Renders a single ad
-function renderAdvertisement(wrapper) {
-  let div = document.createElement("div");
-  div.classList.add("ad-faq");
-  div.style.backgroundImage = `url("../Advertising-images/${randomizeAdNumber()}.png")`;
-  wrapper.appendChild(div);
+function renderAdvertisement (wrapper) {
+  let div = document.createElement('div')
+  div.classList.add('ad-faq')
+  div.style.backgroundImage = `url("../Advertising-images/${randomizeAdNumber()}.png")`
+  wrapper.appendChild(div)
 }
 
 // Randomize a number from 1-11
-function randomizeAdNumber() {
-  let nr = Math.floor(Math.random() * 11) + 1;
-  console.log(nr);
+function randomizeAdNumber () {
+  let nr = Math.floor(Math.random() * 11) + 1
+  console.log(nr)
   return nr
 }
 
-
+//DIRECT CODE
 renderQuestions()
--- JS/homepage.js
+++ JS/homepage.js
@@ -1,50 +1,51 @@
-//function for building menu
+'use strict'
 
-let body = document.querySelector("body");
-let background = document.getElementById("background");
+let body = document.querySelector('body')
+let background = document.getElementById('background')
 
-
+//Function for creating the outline for background image(s) and if statement to check current time, if:
+//a) Time is less than 6 p.m. and more than 7 a.m. - show DAY GIF
+//b) Time is more than 6 p.m. and less than 7 a.m. - show NIGHT GIF.
 function setBackground () {
-    
-    let bgDiv = document.createElement("div");
-    background.appendChild(bgDiv);
+  let bgDiv = document.createElement('div')
+  background.appendChild(bgDiv)
 
-    var d = new Date();
-    var hours = d.getHours();
+  var d = new Date()
+  var hours = d.getHours()
 
-    if (hours <= 18 && hours >= 7) {
-        bgDiv.innerHTML = `<img src="../Images/Startpage_GIFs/day.gif">`
-    } else {
-        bgDiv.innerHTML =`<img src="../Images/Startpage_GIFs/night.gif">`
-    }
+  if (hours <= 18 && hours >= 7) {
+    bgDiv.innerHTML = `<img src="../Images/Startpage_GIFs/day.gif">`
+  } else {
+    bgDiv.innerHTML = `<img src="../Images/Startpage_GIFs/night.gif">`
+  }
 
-    bgDiv.classList.add("bgImage");
-
+  bgDiv.classList.add('bgImage')
 }
 
+//Function to create shape with text ('bubble') in bottom right corner
 function setBgShape () {
+  let bgShape = document.createElement('div')
 
-    let bgShape = document.createElement("div");
+  bgShape.classList.add('bgShape')
 
-    bgShape.classList.add("bgShape");
-
-    body.appendChild(bgShape);
-
+  body.appendChild(bgShape)
 }
 
+//Function to create icon ('Traveo') in top right corner
 function setIcon () {
-    let icon = document.createElement("div");
+  let icon = document.createElement('div')
 
-    icon.classList.add("icon");
+  icon.classList.add('icon')
 
-    body.appendChild(icon);
+  body.appendChild(icon)
 }
 
+//Function to run all functions at same time
 function runBackground () {
-
-    setBackground();
-    setBgShape();
-    setIcon();
+  setBackground()
+  setBgShape()
+  setIcon()
 }
 
-runBackground();
+//DIRECT CODE
+runBackground()
